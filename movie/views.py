5c82178bb50347f0638d6d5d41f74b48f153702e
Jean Carlo Londoño <jclondonoo@eafit.edu.co>
2025-02-09 20:45:37 -0500

    Cambios en la función about

--- movie/views.py
+++ movie/views.py
@@ -9,4 +9,5 @@ def home(request):
    return render(request, 'home.html', {'name': 'Jean Carlo Londoño Ocampo'})
 
 def about(request):
-    return HttpResponse('<h1>This is the "about" section')
+   # return HttpResponse('<h1>This is the "about" section<h1>')
+   return render(request, 'about.html')
