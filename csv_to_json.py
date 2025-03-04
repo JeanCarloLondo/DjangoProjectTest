7c46f0a917403e1e715fa8899f48dd659c5211dc
Jean Carlo Londoño <jclondonoo@eafit.edu.co>
2025-03-03 21:49:45 -0500

    Dataset de películas

---
+++ csv_to_json.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import json
+
+# Lee el archivo CSV
+df = pd.read_csv('movies_initial.csv')
+
+# Guarda el Dataframe como JSON
+df.to_json('movies_initial.json', orient='records')
+
+with open('movies_initial.json', 'r') as file:
+    movies = json.load(file)
+    
+for i in range(100):
+    movie = movies[i]
+    print(movie)
+    break
